250624e4fa95efdbbb61a2a1795918a8220e7429
ShannonKuo <b03901009@ntu.edu.tw>
2017-05-27 19:22:42 +0800

    map finish

---
+++ dsp_hw3/testdata/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#mv 1.txt orig_1.txt
+#mv 2.txt orig_2.txt
+#mv 3.txt orig_3.txt
+#mv 4.txt orig_4.txt
+#mv 5.txt orig_5.txt
+#mv 6.txt orig_6.txt
+#mv 7.txt orig_7.txt
+#mv 8.txt orig_8.txt
+#mv 9.txt orig_9.txt
+#mv 10.txt orig_10.txt
+#mv example.txt orig_example.txt
+
+perl ../separator_big5.pl orig_1.txt>1.txt
+perl ../separator_big5.pl orig_2.txt>2.txt
+perl ../separator_big5.pl orig_3.txt>3.txt
+perl ../separator_big5.pl orig_4.txt>4.txt
+perl ../separator_big5.pl orig_5.txt>5.txt
+perl ../separator_big5.pl orig_6.txt>6.txt
+perl ../separator_big5.pl orig_7.txt>7.txt
+perl ../separator_big5.pl orig_8.txt>8.txt
+perl ../separator_big5.pl orig_9.txt>9.txt
+perl ../separator_big5.pl orig_10.txt>10.txt
+perl ../separator_big5.pl orig_example.txt>example.txt
